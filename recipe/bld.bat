1f352be3fcfc650a88d348ba5d253394327ee6c8
Isuru Fernando <isuruf@gmail.com>
2024-08-29 09:19:17 -0500

    Fix license and windows

---
+++ recipe/bld.bat
@@ -0,0 +1,11 @@
+set CARGO_FEATURE_EXTERNAL_CPP=1
+set "PSP_ROOT_DIR=%SRC_DIR%/perspective-cpp"
+cd perspective_python-%PKG_VERSION%
+copy ../perspective_python-%PKG_VERSION%.data .
+cd rust/perspective-client
+set CARGO_FEATURE_EXTERNAL_PROTO=1
+cargo build
+cd ../../
+set CARGO_FEATURE_EXTERNAL_PROTO=
+%PYTHON% -m pip install . -vv
+
