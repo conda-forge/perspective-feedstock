f69cbbd782b5eda6257ed22960ea3cd8f24eaaa0
Isuru Fernando <isuruf@gmail.com>
2024-09-09 12:52:14 -0500

    Update bld.bat

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -3,11 +3,17 @@ set "PSP_ROOT_DIR=%SRC_DIR%\\perspective-cpp"
 cd perspective_python-%PKG_VERSION%
 mkdir perspective_python-%PKG_VERSION%.data
 xcopy /s /y %SRC_DIR%\perspective_python-%PKG_VERSION%.data perspective_python-%PKG_VERSION%.data\
+if errorlevel 1 exit 1
+
 cd rust\\perspective-client
 set CARGO_FEATURE_EXTERNAL_PROTO=1
 cargo build
+if errorlevel 1 exit 1
+
 cd ..\\..\\
 set CARGO_FEATURE_EXTERNAL_PROTO=
 %PYTHON% -m pip install . -vv
-%PYTHON% %RECIPE_DIR%\\copy.py
+if errorlevel 1 exit 1
 
+%PYTHON% %RECIPE_DIR%\\copy.py
+if errorlevel 1 exit 1
