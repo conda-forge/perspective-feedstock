605bda02fbc03196c6293c349b62e829a6b9b4b7
Isuru Fernando <isuruf@gmail.com>
2024-09-12 12:25:38 -0500

    set CARGO_TARGET_DIR

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,5 +1,6 @@
 set CARGO_FEATURE_EXTERNAL_CPP=1
 set "PSP_ROOT_DIR=%SRC_DIR%\\perspective-cpp"
+set "CARGO_TARGET_DIR=D:\psp-rust"
 cd perspective_python-%PKG_VERSION%
 mkdir perspective_python-%PKG_VERSION%.data
 xcopy /s /y %SRC_DIR%\perspective_python-%PKG_VERSION%.data perspective_python-%PKG_VERSION%.data\
