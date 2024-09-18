532b8d78682338206050928767aba4a6490e8480
Tom "Codespace" Jakubowski <tom@prospective.dev>
2024-09-18 02:07:53 +0000

    perspective 3.1.0, from git source
    includes a number of patches which we will attempt upstreaming to
    perspective:
    - remove various postinstall hooks (maybe not upstreamable)
    - use header-only boost
    - support non-abi3 build
    - explicit selection of python interpreter for maturin
    - use conda-provided protoc (protobuf-src crate doesn't build in
      cross-compiling conda environment)

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,20 +1,44 @@
-set CARGO_FEATURE_EXTERNAL_CPP=1
-set "PSP_ROOT_DIR=%SRC_DIR%\\perspective-cpp"
+@REM set CARGO_FEATURE_EXTERNAL_CPP=1
+@REM set "PSP_ROOT_DIR=%SRC_DIR%\\perspective-cpp"
+@REM set "CARGO_TARGET_DIR=D:\psp-rust"
+@REM cd perspective_python-%PKG_VERSION%
+@REM mkdir perspective_python-%PKG_VERSION%.data
+@REM xcopy /s /y %SRC_DIR%\perspective_python-%PKG_VERSION%.data perspective_python-%PKG_VERSION%.data\
+@REM if errorlevel 1 exit 1
+
+@REM cd rust\\perspective-client
+@REM set CARGO_FEATURE_EXTERNAL_PROTO=1
+@REM cargo build
+@REM if errorlevel 1 exit 1
+
+@REM cd ..\\..\\
+@REM set CARGO_FEATURE_EXTERNAL_PROTO=
+@REM %PYTHON% -m pip install . -vv
+@REM if errorlevel 1 exit 1
+
+@REM %PYTHON% %RECIPE_DIR%\\copy.py
+@REM if errorlevel 1 exit 1
+
+@REM protobuf-src is patched out of the build.  its build script fails to link in
+@REM an osx cross-compiling environment; the wrong toolchain is used.
+
+set "PROTOC="
+for /f %%i in ('where protoc') do if not defined PROTOC set "PROTOC=%%i"
+if not defined PROTOC echo "protoc not found" & exit /b 1
+
+set PSP_BUILD_WHEEL=1
+set PSP_ARCH=x86_64
+set PACKAGE=perspective-python
+set "PSP_ROOT_DIR=%SRC_DIR%"
+@REM If your build is hosted on a non-D: drive, like at C:\bld, you may need to adjust the
+@REM CARGO_TARGET_DIR value to use the same drive as the build, or else you may encounter an error like this:
+@REM Error: Os { code: 17, kind: CrossesDevices, message: "The system cannot move the file to a different disk drive." }
 set "CARGO_TARGET_DIR=D:\psp-rust"
-cd perspective_python-%PKG_VERSION%
-mkdir perspective_python-%PKG_VERSION%.data
-xcopy /s /y %SRC_DIR%\perspective_python-%PKG_VERSION%.data perspective_python-%PKG_VERSION%.data\
-if errorlevel 1 exit 1
+call pnpm install --filter "@finos/perspective-python"
+call pnpm run build
+if errorlevel 1 exit /b 1
 
-cd rust\\perspective-client
-set CARGO_FEATURE_EXTERNAL_PROTO=1
-cargo build
-if errorlevel 1 exit 1
+for %%w in (%CARGO_TARGET_DIR%\wheels\perspective_python*.whl) do "%PYTHON%" -m pip install "%%w" -vv
+if errorlevel 1 exit /b 1
 
-cd ..\\..\\
-set CARGO_FEATURE_EXTERNAL_PROTO=
-%PYTHON% -m pip install . -vv
-if errorlevel 1 exit 1
-
-%PYTHON% %RECIPE_DIR%\\copy.py
-if errorlevel 1 exit 1
+echo "They can build perspective. Why can't they make a cup of coffee that tastes good?"
--- recipe/build.sh
+++ recipe/build.sh
@@ -1,11 +1,47 @@
-export CARGO_FEATURE_EXTERNAL_CPP=1
-export PSP_ROOT_DIR=$SRC_DIR/perspective-cpp
-cd perspective_python-${PKG_VERSION}
-cp -r ../perspective_python-${PKG_VERSION}.data .
-cd rust/perspective-client
-export CARGO_FEATURE_EXTERNAL_PROTO=1
-cargo build
-cd ../../
-unset CARGO_FEATURE_EXTERNAL_PROTO
-${PYTHON} -m pip install . -vv
-${PYTHON} ${RECIPE_DIR}/copy.py
+#!/bin/bash
+set -ex
+
+# XXX(tom): don't understand why it's needed to set PKG_CONFIG_PATH.
+# this directory is already present in pkg-config's config path as reported by
+# `pkg-config --variable pc_path pkg-config` in a --debug build environment.
+# and yet `pkg-config <some-dependency>` fails
+export PKG_CONFIG_PATH=$BUILD_PREFIX/lib/pkgconfig/:$PKG_CONFIG_PATH
+
+case "${target_platform}" in
+linux-aarch64 | osx-arm64)
+  arch="aarch64"
+  ;;
+*)
+  arch="x86_64"
+  ;;
+esac
+export PSP_ARCH=$arch
+
+echo "PSP_ARCH: $arch"
+
+pnpm install --filter '@finos/perspective-python'
+
+# Run perspective git-source build
+export PACKAGE=perspective-python
+export PSP_BUILD_WHEEL=1
+export PROTOC
+# protobuf-src is patched out of the build.  its build script fails to link in
+# an osx cross-compiling environment; the wrong toolchain is used.
+PROTOC=$(which protoc)
+pnpm run build
+
+# Install wheel to site-packages ($SP_DIR), wherefrom Conda assembles the .conda package contents
+$PYTHON -m pip install rust/target/wheels/perspective_python*.whl -vv
+
+# old recipe:
+# export CARGO_FEATURE_EXTERNAL_CPP=1
+# export PSP_ROOT_DIR=$SRC_DIR/perspective-cpp
+# cd perspective_python-${PKG_VERSION}
+# cp -r ../perspective_python-${PKG_VERSION}.data .
+# cd rust/perspective-client
+# export CARGO_FEATURE_EXTERNAL_PROTO=1
+# cargo build
+# cd ../../
+# unset CARGO_FEATURE_EXTERNAL_PROTO
+# ${PYTHON} -m pip install . -vv
+# ${PYTHON} ${RECIPE_DIR}/copy.py
